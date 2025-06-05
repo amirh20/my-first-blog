91188b4adc417fe422e1e0c265cff7bbc397952c
amir <amirlesani77@gmail.com>
2025-06-05 13:20:41 +0330

    nothin

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,3 +14,7 @@ class Post(models.Model):
         self.save()
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"id":self.id})
+    
+    
