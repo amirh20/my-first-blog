1400e0ccdb8abca5ff2fc4196f4d8f3809eb5acd
amir <amirlesani77@gmail.com>
2025-06-06 00:41:11 +0330

    viel

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ app_name='blog'
 urlpatterns=[
     path('',views.PostListView.as_view(),name='post_list'),
     path('blog/<int:id>/',views.PostDetailView.as_view(),name='post_detail'),
+    path('blog/post/new/',views.PostNewView.as_view(),name='post_new' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views import View
 from .models import Post 
 from django.utils import timezone
+from .forms import PostForm
 
 class PostListView(View):
     def get(self,request):
@@ -12,3 +13,17 @@ class PostDetailView(View):
     def get(self,request,id):
         post=get_object_or_404(Post,pk=id)
         return render(request,'blog/post_detail.html',{'post':post})
+
+class PostNewView(View):
+    def get(self,request):
+        form=PostForm()
+        return render(request,'blog/post_new.html',{'form':form})
+    def post(self,request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.published_date=timezone.now()
+            post.save()
+            return redirect(post.get_absolute_url())
+        return render(request,'blog/post_new.html',{'form':form})
